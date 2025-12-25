2cdd46e4be39c456ffaffda4c2c013587158e68f
Pedro Brum <pedrobrumtc@gmail.com>
2025-12-25 19:17:02 -0300

    Correção do nó de controle

--- scripts/controle.py
+++ scripts/controle.py
@@ -3,9 +3,7 @@
 
 
 import rospy
-import math
 
-import tf
 from std_msgs.msg import Float32MultiArray
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
@@ -32,10 +30,6 @@ class Controle:
         # ===== Parâmetros =====
 
         # Constantes PID 
-        self.kp_yaw = 1.0
-        self.ki_yaw = 0.0
-        self.kd_yaw = 0.0
-
         self.kp_depth = 1.0
         self.ki_depth = 0.0
         self.kd_depth = 0.0
@@ -66,7 +60,7 @@ class Controle:
 
     def compute_control(self):
         """
-        Implementa o controle PID para yaw e profundidade.
+        Implementa o controle PID para profundidade.
         Calcula os sinais de controle.
         """
         
@@ -76,66 +70,47 @@ class Controle:
         if dt == 0:
             return
         
-        # Extrai estado atual
-        current_yaw = self.get_yaw_from_odometry(self.state)
-        current_depth = self.state.pose.pose.position.z
 
-        # Extrai comandos desejados
-        desired_yaw = self.cmd.angular.z
+        current_depth = self.state.pose.pose.position.z
         desired_depth = self.cmd.linear.z
 
         # Calcula erros
-        yaw_error = desired_yaw - current_yaw
         depth_error = desired_depth - current_depth
-
-        # Integração dos erros
-        self.yaw_error_i += yaw_error * dt
         self.depth_error_i += depth_error * dt
-
-        # Derivada dos erros
-        yaw_error_d = yaw_error / dt
         depth_error_d = depth_error / dt
 
         # Sinais de controle PID
-        yaw_control = (self.kp_yaw * yaw_error +
-                       self.ki_yaw * self.yaw_error_i +
-                       self.kd_yaw * yaw_error_d)
         
-        depth_control = (self.kp_depth * depth_error +
-                         self.ki_depth * self.depth_error_i +
-                         self.kd_depth * depth_error_d)
+        depth_control = self.kp_depth * depth_error + self.ki_depth * self.depth_error_i + self.kd_depth * depth_error_d
         
-        return [yaw_control, depth_control]
+        return depth_control
 
-    def distribute_motor_commands(self, yaw_control, depth_control):
+    def distribute_motor_commands(self, forward, rotate, depth_control):
         """
         Distribui os sinais de controle para os motores do ROV.
         """
-        motor_cmd = Float32MultiArray()
-
-        # exemplo basico com 4 motores (2 para profundidade e 2 para avançar e rotacionar)
-        # Falta o sinal de avanço do ROV
-        motor_cmd.data = [depth_control,
-                          depth_control,
-                          yaw_control,
-                          -yaw_control]
-        return motor_cmd
         
-    def get_yaw_from_odometry(self, odom):
-        """
-        Extrai o ângulo yaw (em radianos) da mensagem de odometria.
-        """
-        orientation_q = odom.pose.pose.orientation
-        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(orientation_list)
-        return yaw
+        if rotate != 0 and forward != 0:
+            left = (3*forward + rotate)/3
+            right = (3*forward - rotate)/3
+        else:
+            left = forward + rotate
+            right = forward - rotate
     
+        motor_cmd = Float32MultiArray()
+        motor_cmd.data = [left, right, depth_control] 
+        # Falta o sinal de avanço do ROV
+        
+        return motor_cmd
+
     # ================= LOOP =================
 
     def run(self):
         while not rospy.is_shutdown():
-            yaw_control,depth_control = self.compute_control()     
-            motors = self.distribute_motor_commands(yaw_control, depth_control)      
+            forward = self.cmd.linear.x
+            rotate = self.cmd.angular.z
+            depth_control = self.compute_control()
+            motors = self.distribute_motor_commands(forward, rotate, depth_control)      
             self.motor_pub.publish(motors) 
             self.rate.sleep()
 
